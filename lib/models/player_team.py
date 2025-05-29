ec20a88d7647e5a1d3998e7dacc3fd056c018903
Neema <neema.edward@student.moringaschool.com>
2025-05-29 07:12:00 +0300

    updates the player_team file

---
+++ lib/models/player_team.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, ForeignKey, Table
+from lib.models import Base
+
+player_teams = Table(
+    'player_teams',
+    Base.metadata,
+    Column('player_id', Integer, ForeignKey('players.id'), primary_key=True),
+    Column('team_id', Integer, ForeignKey('teams.id'), primary_key=True)
+)
